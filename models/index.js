4bf971bfd2f7e089eb53fe74b2bbbbcd99fbd5b0
John Boyle <nikolasboyle@gmail.com>
2022-06-29 15:19:07 -0700

    Update sequelize connection config

--- models/index.js
+++ models/index.js
@@ -7,10 +7,12 @@ const { getVoteModel } = require("./vote.model");
 const { getViewModel } = require("./view.model");
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
+  process.env.DATABASE_NAME,
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     dialect: "postgres",
     logging: false,
   }
